d7323e974ec50a739a1adc809ca04db97572d89e
Chaimy <chaim@szstudios.net>
2010-10-06 23:33:17 -0400

    file count land on water added

---
+++ matrix_land.rb
@@ -0,0 +1,42 @@
+M = 'land'
+o = 'water'
+
+world = [[o,o,o,o,o,o,o,o,o,o,o],
+         [o,o,o,o,M,M,o,o,o,o,o],
+         [o,o,o,o,o,o,o,o,M,M,o],
+         [o,o,o,M,o,o,o,o,o,M,o],
+         [o,o,o,M,o,M,M,o,o,o,o],
+         [o,o,o,o,M,M,M,M,o,o,o],
+         [o,o,o,M,M,M,M,M,M,M,o],
+         [o,o,o,M,M,o,M,M,M,o,o],
+         [o,o,o,o,o,o,M,M,o,o,o],
+         [o,M,o,o,o,M,o,o,o,o,o],
+         [o,o,o,o,o,o,o,o,o,o,o]]
+         
+def continent_size world, x, y
+  if world[x][y] != 'land'
+    return 0
+  end
+  
+  
+  size = 1
+  world[x][y] = 'counted land'
+  
+  
+  size = size + continent_size(world, x-1, y-1)
+  size = size + continent_size(world, x  , y-1)
+  size = size + continent_size(world, x+1, y-1)
+  size = size + continent_size(world, x-1, y  )
+  size = size + continent_size(world, x+1, y  )
+  size = size + continent_size(world, x-1, y+1)
+  size = size + continent_size(world, x  , y-1)
+  size = size + continent_size(world, x+1, y-1)
+  
+  
+  size
+end
+
+
+puts continent_size(world, 5, 5)
+  
+  
